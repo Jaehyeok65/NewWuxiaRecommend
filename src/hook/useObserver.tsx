ceda5b82411b818fc7f4503b4468acb809fbcdc5
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-06-23 20:09:45 +0900

    feat : useObserver, useScroll 훅 추가

---
+++ src/hook/useObserver.tsx
@@ -0,0 +1,35 @@
+import { useRef, useCallback, useEffect } from 'react';
+
+const useObserver = (hasNextPage: boolean, fetchNextPage: any) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const target = useRef<NodeJS.Timeout | null>(null);
+
+    const observerCallback = useCallback(
+        ([entries]: any) => {
+            if (entries.isIntersecting && hasNextPage) {
+                if (!target.current) {
+                    target.current = setTimeout(() => {
+                        target.current = null;
+                    }, 500);
+                    fetchNextPage();
+                }
+            }
+        },
+        [hasNextPage, fetchNextPage]
+    );
+
+    const option = { threshold: 0.25, rootMargin: '80px'};
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        const observer = new IntersectionObserver(observerCallback, option);
+        observer.observe(ref.current);
+
+        return () => observer.disconnect();
+    }, [ref, option, useCallback]);
+    
+    return ref;
+};
+
+export default useObserver;
