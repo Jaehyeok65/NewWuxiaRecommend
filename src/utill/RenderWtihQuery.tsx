9a4bd1a649cb04b094adf5e8ff71df2556e24e82
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-07-14 19:22:05 +0900

    test : 테스트 설정

---
+++ src/utill/RenderWtihQuery.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootreducer from 'redux/reducers';
+
+export const RenderWithProviders = ({ children, route = '/' }: any) => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                // ✅ turns retries off
+                retry: false,
+            },
+        },
+    });
+
+    const store = createStore(rootreducer, {});
+
+    return (
+        <Provider store={store}>
+            <MemoryRouter initialEntries={[route]}>
+                <QueryClientProvider client={queryClient}>
+                    <Routes>{children}</Routes>
+                </QueryClientProvider>
+            </MemoryRouter>
+        </Provider>
+    );
+};
