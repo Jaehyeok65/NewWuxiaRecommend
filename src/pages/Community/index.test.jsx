3ea3c46aa90b5e65fa2d7225b7f03faa71640d21
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-07-27 19:13:48 +0900

    test : Community 컴포넌트 테스트 추가

--- src/pages/Community/index.test.jsx
+++ src/pages/Community/index.test.jsx
@@ -1,52 +1,163 @@
-import { fireEvent, screen, RouterRender, render } from "../../util/test";
-import Community from ".";
+import { RenderWithProviders } from 'utill/RenderWtihQuery';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Routes, Route } from 'react-router-dom';
+import Community from './index';
+import MockAdapter from 'axios-mock-adapter';
+import { API } from 'api/LoginAPI';
+import axios from 'axios';
+import CommentWrite from 'pages/CommentWrite';
 
+const mockNavigate = jest.fn();
 
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
 
+describe('Community Component Test', () => {
+    const mock = new MockAdapter(axios);
 
-describe('Presentational Component Test', () => {
-
-    const data = [{
-        id : 2,
-        title : '제목이 뭐니',
-        content : '내용이 뭐니',
-        writer : '호돌맨',
-        date : '2022-10-27',
-        view : 0,
-        recommend : 0
-      }];
-
-    it('데이터가 있으며 현재 페이지 정보가 있으면 리스트가 성공적으로 보인다', () => {
-        render(<Community data={data} offset={0} limit={10} /> );
-        screen.getByText(data[0].title);
+    afterEach(() => {
+        mock.reset(); //테스트 케이스를 수행 후 mock 데이터를 초기화함
     });
 
-    it('로그인 정보가 없는데 글쓰기 버튼을 누르면 login창이 나타난다.', async() => {
-        const isLoginToggle = jest.fn();
-        render(<Community data={data} offset={0} limit={10} isLoginToggle={isLoginToggle} /> );
-        const Icon = screen.getByTestId('write');
-        fireEvent.click(Icon);
-        expect(isLoginToggle).toBeCalled();
+    const commentdata = [
+        {
+            title: '백련성신',
+            content: '재밌다',
+            writer: '은사해탈',
+            date: '2024-07-26',
+            view: 10,
+            recommend: 0,
+        },
+    ];
+
+    const alertMock = jest.fn();
+    window.alert = alertMock;
+
+    it('loginstate가 false일 때 글을 작성할 경우 경고 메시지가 발생힌다.', async () => {
+        mock.onPost(`${API}/commentlist`, { title: '최신순' }).reply(
+            200,
+            commentdata
+        );
+
+        render(
+            <RenderWithProviders route="/community">
+                <Routes>
+                    <Route
+                        path="/community"
+                        element={<Community loginstate={false} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('백련성신', {}, { timeout: 5000 })
+        ).toBeInTheDocument();
+
+        const pen = screen.getByTestId('nologinwrite');
+
+        expect(pen).toBeInTheDocument();
+
+        fireEvent.click(pen);
+
+        await waitFor(() =>
+            expect(alertMock).toHaveBeenCalledWith(
+                '로그인이 필요한 기능입니다.'
+            )
+        );
+
+        expect(mockNavigate).toHaveBeenCalledWith('/login');
     });
 
-    it('page 정보가 있으면 pagenation 컴포넌트가 보인다.', async() => {
-        render(<Community data={data} offset={0} limit={10} page={1} />);
-        screen.getByText('<');
-        screen.getByText('1');
-        screen.getByText('>');
+    it('loginstate가 true일 때 글을 작성할 경우 commentwrite 페이지로 이동한다.', async () => {
+        mock.onPost(`${API}/commentlist`, { title: '최신순' }).reply(
+            200,
+            commentdata
+        );
+
+        render(
+            <RenderWithProviders route="/community">
+                <Routes>
+                    <Route
+                        path="/community"
+                        element={<Community loginstate={true} />}
+                    />
+                    <Route
+                        path="/commentwrite"
+                        element={
+                            <CommentWrite
+                                loginstate={true}
+                                nickname="팔협지"
+                                지
+                            />
+                        }
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('백련성신', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+
+        const pen = screen.getByTestId('loginwrite');
+
+        expect(pen).toBeInTheDocument();
+
+        fireEvent.click(pen);
+
+        expect(
+            await screen.findByTestId('loading-spinner')
+        ).toBeInTheDocument();
+
+        expect(
+            await screen.findByPlaceholderText(
+                '제목을 입력하세요...',
+                {},
+                { timeout: 3000 }
+            )
+        ).toBeInTheDocument();
     });
 
-    it('SelectList props가 있으면 selectList가 보이며, select를 변경하면 selected가 변경된다.', () => {
-        const handleSelect = jest.fn();
-        const selectList = ["최신순", "추천순"];
-        const Selected = '최신순';
-        render(<Community data={data} offset={0} limit={10} page={1} handleSelect={handleSelect} selectList={selectList} Selected={Selected} /> ); 
-        const selection = screen.getByDisplayValue(Selected);
+    it('추천순으로 바꿀 경우 페이지가 1로 초기화된다.', async () => {
+        mock.onPost(`${API}/commentlist`, { title: '최신순' }).reply(
+            200,
+            commentdata
+        );
+
+        mock.onPost(`${API}/commentlist`, { title: '추천순' }).reply(
+            200,
+            commentdata
+        );
+
+
+        render(
+            <RenderWithProviders route="/community">
+                <Routes>
+                    <Route
+                        path="/community"
+                        element={<Community loginstate={true} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('백련성신', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+
+        const selection = screen.getByDisplayValue('최신순');
         fireEvent.change(selection, { target : { value : '추천순' }});
-        expect(handleSelect).toBeCalled();
+
+        expect(await screen.findByText('1')).toBeInTheDocument();
+
     });
-
-    
-
-
-})
+});
