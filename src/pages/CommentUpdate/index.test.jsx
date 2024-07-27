99fd97df175e641616f44c8b6b9fe63332d3a16a
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-07-27 20:05:47 +0900

    test : CommentUpdate 컴포넌트 테스트 추가

--- src/pages/CommentUpdate/index.test.jsx
+++ src/pages/CommentUpdate/index.test.jsx
@@ -1,73 +1,189 @@
 import React from 'react';
-import { API, create, screen, fireEvent, RouterRender } from "../../util/test";
-import CommentUpdate from '.';
+import CommentUpdate from './index';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { Provider } from 'react-redux';
+import { API } from 'api/LoginAPI';
+import { RenderWithProviders } from 'utill/RenderWtihQuery';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Routes, Route } from 'react-router-dom';
 
+const data = {
+    id: 2,
+    title: '제목을 테스트',
+    content: '내용 테스트',
+    writer: '호돌맨',
+    data: '2022-10-27',
+    view: 0,
+    recommend: 0,
+};
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
 
 
 describe('CommentUpdate Test', () => {
+    const mock = new MockAdapter(axios); // 200ms 가짜 딜레이 설정
 
-    const mock = new MockAdapter(axios, { delayResponse: 200 }); // 200ms 가짜 딜레이 설정
+    afterEach(() => {
+        mock.reset(); //테스트 케이스를 수행 후 mock 데이터를 초기화함
+    });
 
-    const data = {
-      id : 2,
-      title : '제목을 테스트',
-      content : '내용 테스트',
-      writer : '호돌맨',
-      data : '2022-10-27',
-      view : 0,
-      recommend : 0
-    };
-
-
-    mock.onPost(`${API}/comment`).reply(200, data);
-
-    
-    const { store } = create();
+    const alertMock = jest.fn();
+    window.alert = alertMock;
 
 
     it('Comment가 있다면, Comment 입력창이 보이며, 정상적으로 value가 업데이트 되어있다.', async () => {
-        RouterRender(
-        <Provider store={store}>
-            <CommentUpdate loginstate={true} />
-        </Provider>, {
-            path : '/commentupdate/:id',
-            initial : '/commentupdate/2'
-        });
+        mock.onPost(`${API}/comment`).reply(200, data);
 
-       await screen.findByDisplayValue('내용 테스트');
-       await screen.findByDisplayValue('제목을 테스트');
-    })
+        render(
+            <RenderWithProviders route="/commentupdate/2">
+                <Routes>
+                    <Route
+                        path="/commentupdate/:id"
+                        element={<CommentUpdate loginstate={true} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
 
-    it('Comment가 있다면, Comment 입력창이 보이며, Input value를 변경했을 때, 정상적으로 변경이 된다.', async () => {
-        RouterRender(
-        <Provider store={store}>
-            <CommentUpdate loginstate={true} />
-        </Provider>, {
-            path : '/commentupdate/:id',
-            initial : '/commentupdate/2'
-        });
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
 
-       const title = await screen.findByDisplayValue('내용 테스트');
-       const content = await screen.findByDisplayValue('제목을 테스트');
-       fireEvent.change(title, { target : { value : '제목 변경'}});
-       fireEvent.change(content, { target : { value : '내용 변경'}});
-       expect(title.value).toBe('제목 변경');
-       expect(content.value).toBe('내용 변경');
-    })
+        expect(
+            await screen.findByText('내용 테스트', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+    });
 
-    it('Comment가 있다면, Comment 입력창이 보이며, 버튼을 눌렀을 때 Submit이 정상적으로 작동이 된다.', async () => {
-        RouterRender(
-        <Provider store={store}>
-            <CommentUpdate loginstate={true} />
-        </Provider>, {
-            path : '/commentupdate/:id',
-            initial : '/commentupdate/2'
-        });
+    it('loginstate가 false라면 경고메시지가 발생하며 로그인 페이지로 이동된다.', async () => {
+        mock.onPost(`${API}/comment`).reply(200, data);
 
-        const btn = await screen.findByDisplayValue('전송');
-        fireEvent.click(btn);
-    })
-})
+        render(
+            <RenderWithProviders route="/commentupdate/2">
+                <Routes>
+                    <Route
+                        path="/commentupdate/:id"
+                        element={<CommentUpdate loginstate={false} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        await waitFor(() =>
+            expect(alertMock).toHaveBeenCalledWith(
+                '로그인이 필요합니다.'
+            )
+        );
+
+        expect(mockNavigate).toHaveBeenCalledWith('/login');
+
+    });
+
+    it('제목을 입력하지 않고 전송 버튼을 누를 시 경고메시지가 발생한다.', async () => {
+        mock.onPost(`${API}/comment`).reply(200, data);
+
+        render(
+            <RenderWithProviders route="/commentupdate/2">
+                <Routes>
+                    <Route
+                        path="/commentupdate/:id"
+                        element={<CommentUpdate loginstate={true} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('내용 테스트', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+
+        const titleinput = screen.getByTestId('titleinput');
+
+        fireEvent.change(titleinput, { target : { value : ''}});
+
+        const submitbtn = screen.getByText('전송');
+
+        expect(submitbtn).toBeInTheDocument();
+
+        fireEvent.click(submitbtn);
+
+        await waitFor(() =>
+            expect(alertMock).toHaveBeenCalledWith(
+                '제목을 입력하세요'
+            )
+        );   
+    });
+
+    it('내용을 입력하지 않고 전송 버튼을 누를 시 경고메시지가 발생한다.', async () => {
+        mock.onPost(`${API}/comment`).reply(200, data);
+
+        render(
+            <RenderWithProviders route="/commentupdate/2">
+                <Routes>
+                    <Route
+                        path="/commentupdate/:id"
+                        element={<CommentUpdate loginstate={true} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('내용 테스트', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+
+        const contentinput = screen.getByTestId('contentinput');
+
+        fireEvent.change(contentinput, { target : { value : ''}});
+
+        const submitbtn = screen.getByText('전송');
+
+        expect(submitbtn).toBeInTheDocument();
+
+        fireEvent.click(submitbtn);
+
+        await waitFor(() =>
+            expect(alertMock).toHaveBeenCalledWith(
+                '내용을 입력하세요'
+            )
+        );   
+    });
+
+    it('내용과 제목을 모두 입력하고 전송 버튼을 누를 시 데이터 fetch가 성공하면 comment 페이지로 이동된다.', async () => {
+        mock.onPost(`${API}/comment`).reply(200, data);
+
+        render(
+            <RenderWithProviders route="/commentupdate/2">
+                <Routes>
+                    <Route
+                        path="/commentupdate/:id"
+                        element={<CommentUpdate loginstate={true} />}
+                    />
+                </Routes>
+            </RenderWithProviders>
+        );
+
+        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+
+        expect(
+            await screen.findByText('내용 테스트', {}, { timeout: 3000 })
+        ).toBeInTheDocument();
+
+        const submitbtn = screen.getByText('전송');
+
+        expect(submitbtn).toBeInTheDocument();
+
+        fireEvent.click(submitbtn);
+
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/comment/2'));
+    });
+
+});
