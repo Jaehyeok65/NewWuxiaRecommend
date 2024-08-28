47a192e4cea3ee275d90ee090de6e453173ae19d
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-08-28 20:56:32 +0900

    test : Modal 컴포넌트 테스트 수정

--- src/molecule/Modal/index.test.jsx
+++ src/molecule/Modal/index.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "../../util/test";
+import { render, screen } from "@testing-library/react";
 import Modal from ".";
 
 
@@ -12,6 +12,15 @@ describe('Modal Component Test', () => {
         const content = screen.getByText('내용');
 
         expect(content).toBeInTheDocument();
+    });
+
+    it('toggle Props가 False라면 chilrdren 내용이 보이지 않는다', () => {
+
+        render(<Modal toggle={false}>내용</Modal>);
+
+        const content = screen.queryByText('내용');
+
+        expect(content).not.toBeInTheDocument();
     });
 
 });
