bcc15d4c5ad22d3f8a1c9785cfd544d3c1b7ec4b
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-08-30 15:56:29 +0900

    test : 테스트 render 환경 설정

--- src/utill/RenderWithProvider.tsx
+++ src/utill/RenderWithProvider.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MemoryRouter, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootreducer from 'redux/reducers';
 
-export const RenderWithProviders = ({ children, route = '/' }: any) => {
+export const RenderWithProvider = ({ children, route = '/' }: any) => {
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
@@ -19,11 +18,9 @@ export const RenderWithProviders = ({ children, route = '/' }: any) => {
 
     return (
         <Provider store={store}>
-            <MemoryRouter initialEntries={[route]}>
                 <QueryClientProvider client={queryClient}>
                     {children}
                 </QueryClientProvider>
-            </MemoryRouter>
         </Provider>
     );
 };
