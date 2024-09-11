e15410c0dd0caa1c6f3a45ae78cc81909b4c81dd
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-09-11 19:31:34 +0900

    refactor : 타입 수정

--- src/type/type.tsx
+++ src/type/type.tsx
@@ -1,21 +1,21 @@
-export interface Wuxia {
-    id?: number;
-    title?: string;
-    writer?: string;
-    content?: string;
-    url?: string;
-    view?: number;
-    likes?: number;
-    rate?: number;
-    people?: number;
-    link?: string;
+export interface Wuxia { //Wuxia 
+    id?: number; //아이디는 생성 때 생길 수 있으므로
+    title: string; 
+    writer: string;
+    content: string;
+    url: string;
+    view?: number; //0일 수 있으므로
+    likes?: number; //0일 수 있으므로
+    rate?: number; //0일 수 있으므로
+    people?: number; //0일 수 잇으므로
+    link: string;
 }
 
 export interface WuxiaComment {
     id?: number;
     wuxia?: Wuxia;
-    content?: string;
-    createdAt?: string;
+    content: string;
+    createdAt: string;
     recommendation?: number;
     user?: User;
     user_id:string; //쿠키로 가져오므로 string
